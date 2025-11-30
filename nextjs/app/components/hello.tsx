5decb0d5f23d58041d52172bc5e834358a32bc03
emon22830 <mdemon22830@gmail.com>
2025-11-30 19:01:34 +0800

    nxt.js

---
+++ nextjs/app/components/hello.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+function Hello() {
+  return (
+    <div>
+      <h1>Bro</h1>
+    </div>
+  );
+}
+
+export default Hello;
---
+++ nextjs/app/page.tsx
@@ -0,0 +1,13 @@
+import Hello from "./components/hello";
+
+
+export default function Home(){
+  console.log("What am I ")
+  return(
+  <>
+
+  <h1 className="text-xl">Welcome to Next.js</h1>
+  <Hello/>
+  </>
+  );
+}
